14a7a566b39b88ea49a964598f41ab07c5929bec
ca-m <deconnick.carmel@gmail.com>
2023-02-08 01:14:15 -0800

    include

---
+++ shell.h
@@ -0,0 +1,14 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdio.h> /*printf*/
+#include <unistd.h> /*fork, execve*/
+#include <string.h> /* strtok*/
+#include <errno.h> /*errno and perror*/
+#include <sys/types.h> /*type pid*/
+#include <sys/wait.h> /*wait*/
+#include <sys/stat.h> /*use of stat function*/
+#include <signal.h> /*signal management*/
+#include <fcntl.h> /* open files*/
